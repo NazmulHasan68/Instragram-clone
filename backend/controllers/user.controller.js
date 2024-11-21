3516118b321eaa3dceffd8e9e36ba2584da8cb2d
NazmulHasan68 <aishadowzone@gmail.com>
2024-11-21 20:45:31 +0600

    Complete User Controller Also follow and unfollow logic

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,6 +2,7 @@ import { User } from "../models/user.model.js"
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import { getDataUri } from "../util/datauri.js"
+import cloudinary from "./util/cloudinary.js";
 
 export const register = async(req, res)=>{
     try {
@@ -84,10 +85,71 @@ export const editProfile = async(req, res)=>{
         let cloudResponse;
         if(profilePicture) {
             const fileUri = getDataUri(profilePicture)
-            await 
+            cloudResponse = await cloudinary.uploader.upload(fileUri);
         }
+
+        const user = await User.findById(userId);
+        if(!user) return res.status(404).json({success:false, message:"user not found!"})
+
+        if(bio) user.bio = bio
+        if(gender) user.gender = gender
+        if(profilePicture) user.profilePicture = cloudResponse.secure_url;
+
+        await user.save()
+        return res.status(200).json({success:true, user,  message:"profile updated!"})
+
     } catch (error) {
         res.status(500).json({message:"edit profile error", error})
     }
 }
 
+
+
+
+
+export const getSuggestedUsers = async(req, res)=>{
+    try {
+        const suggestedUsers = await User.find({_id:{$ne:req.id}}).select("-password")
+        if(!suggestedUsers) return res.status(400).json({success: false, message:"Currently do not have any users"})
+        
+        return res.status(201).json({success: true, users:suggestedUsers, message:"Get suggested user Successfully!"})
+    } catch (error) {
+        res.status(500).json({message:"suggested user error", error})
+    }
+}
+
+
+
+export const followOrUnfollow = async(req, res) =>{
+    try {
+        const followerKrneWala = req.id
+        const jiskoFollowkrunga = req.params.id
+
+        if(followOrUnfollow === jiskoFollowkrunga){
+            return res.status(400).json({success:false, message:"You cannot follow /unfollow your self"})
+        }
+
+        const user = await User.findById(followerKrneWala)
+        const targetUser = await User.findById(jiskoFollowkrunga)
+
+        if(!user || !targetUser) return res.status(400).json({success: false, message:'User not found'})
+
+        const isFollowing = user.following.includes(jiskoFollowkrunga)
+        if(isFollowing){
+            await Promise.all([
+                User.updateOne({_id:followerKrneWala}, {$pull:{following:jiskoFollowkrunga}}),
+                User.updateOne({_id:jiskoFollowkrunga}, {$pull:{followers:jiskoFollowkrunga}})
+            ])   
+            return res.status(200).json({success:true, message:"Unfollow successfully!"})
+        }else{
+            await Promise.all([
+                User.updateOne({_id:followerKrneWala}, {$push:{following:jiskoFollowkrunga}}),
+                User.updateOne({_id:jiskoFollowkrunga}, {$push:{followers:jiskoFollowkrunga}})
+            ])
+            return res.status(200).json({success:true, message:"Follow successfully!"})
+        }
+        
+    } catch (error) {
+        res.status(500).json({message:"followOrUnfollow error", error})
+    }
+}
