ee8cde4fffb47a9b2faa8dbeca0ec388f7021fd9
NazmulHasan68 <aishadowzone@gmail.com>
2024-11-22 12:15:36 +0600

    Complete frontend SetUp with shadCn

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import SignUp from './components/SignUp'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+     <Button>Click me</Button>
+     <SignUp/>
+    </>
+  )
+}
+
+export default App
