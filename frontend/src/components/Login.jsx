5aaad515b075c579f395f9fe3d3bd835075cfb80
NazmulHasan68 <aishadowzone@gmail.com>
2024-11-22 23:09:06 +0600

    Redux Compleate setup done

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -6,8 +6,11 @@ import axios from "axios";
 import { toast } from "sonner";
 import { Link, useNavigate } from "react-router-dom";
 import { Loader2 } from "lucide-react";
+import { useDispatch } from "react-redux";
+import { setAuthUser } from "@/redux/authSlice";
 
 export default function Login() {
+  const dispatch = useDispatch()
   const navigate = useNavigate()
   const [input , setInput] = useState({
     email : "",
@@ -31,6 +34,7 @@ export default function Login() {
         withCredentials:true
       })
       if(res.data.success){
+        dispatch(setAuthUser(res.data.user))
         navigate('/')
         toast.success(res.data.message)
         setInput({
