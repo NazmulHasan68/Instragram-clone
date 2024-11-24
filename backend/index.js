38b71a9b70fea4039560943f825068e3e5103d38
NazmulHasan68 <aishadowzone@gmail.com>
2024-11-25 03:27:08 +0600

    Complete setup for deployment

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,9 @@ import userRouter from './routes/user.route.js'
 import postRouter from './routes/post.route.js'
 import messageRouter from './routes/message.route.js'
 import { app , server} from './socket/socket.js'
+import path, { dirname } from 'path'
 dotenv.config()
+const __dirname = path.resolve()
 
 //middleware
 app.use(express.json())
@@ -24,7 +26,13 @@ app.use('/api/v1/user', userRouter)
 app.use('/api/v1/post', postRouter)
 app.use('/api/v1/message', messageRouter)
 
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.get("*", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+})
 const PORT = process.env.PORT || 3000
+
+
 server.listen(PORT, ()=>{
     connectDB()
     console.log(`Server listen at port ${PORT}`);
